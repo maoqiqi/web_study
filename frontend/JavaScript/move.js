ae6bafa9eeac612bb8460ebe85ea1817e601619a
march <march.mao@verystar.cn>
2019-12-24 17:00:10 +0800

    frontend

--- frontend/JavaScript/move.js
+++ frontend/JavaScript/move.js
@@ -3,7 +3,6 @@ function move(obj, attr, target, speed, callback) {
     clearInterval(obj.timer);
     // 获取元素目前的位置
     const current = parseInt(getStyle(obj)[attr]);
-    console.log("current = " + current);
 
     // 判断速度的正负值
     if (current > target) {
@@ -13,7 +12,6 @@ function move(obj, attr, target, speed, callback) {
     // 开启一个定时器,用来执行动画效果
     obj.timer = setInterval(function () {
         let newValue = parseInt(getStyle(obj)[attr]) + speed;
-        console.log("newValue = " + newValue);
         if ((speed < 0 && newValue < target) || (speed > 0 && newValue > target)) {
             newValue = target;
         }
